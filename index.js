85bb995254d93d2e3547254ad2703a58d77af8bd
salcrom <seya.28@gmail.com>
2024-08-22 19:54:53 +0200

    React App ready

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
@@ -23,6 +24,10 @@ app.use(express.json());
 // Rutas
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
+
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
